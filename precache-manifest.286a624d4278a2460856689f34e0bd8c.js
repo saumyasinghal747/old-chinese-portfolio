612fe20c58e094177d16e878c5c89145fe5328d8
FrozenMango747 <realsaumya@gmail.com>
2020-11-06 14:37:11 -0800

    init

---
+++ precache-manifest.286a624d4278a2460856689f34e0bd8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ffd808f9bd44ebd5a8",
+    "url": "/css/chunk-vendors.ca4e4cf1.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "bdc826b8c7b3f8862cbcc6748f4b44d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df9b50a65b325471e242",
+    "url": "/js/about.02c6ec93.js"
+  },
+  {
+    "revision": "3dce9223cec388319dd3",
+    "url": "/js/app.98194115.js"
+  },
+  {
+    "revision": "a6ffd808f9bd44ebd5a8",
+    "url": "/js/chunk-vendors.e85b5375.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
